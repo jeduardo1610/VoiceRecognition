faec738827437e0c096680032628d94679c68919
Jorge Eduardo <jeduardo1601@gmail.com>
2017-08-16 16:27:59 -0500

    Initial Commit

---
+++ VoiceRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceRecognition
+//
+//  Created by Jorge Eduardo on 16/08/17.
+//  Copyright © 2017 Jorge Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
